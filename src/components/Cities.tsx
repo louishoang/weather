9dbbcfc2f33c5f55468e10436ea0b76e670c03c6
Louis Hoang <bawsing@gmail.com>
2018-02-28 15:07:12 -0800

    Create Cities components and connect it to App.tsx

---
+++ src/components/Cities.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Cities extends React.Component {
+  render() {
+    return (
+      <div>
+        List of Cities
+      </div>
+    );
+  }
+}
+
+export default Cities;

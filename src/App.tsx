4e4b254f8d3bd3d7ff3e46cf79e1d7cda6dbb4c4
Louis Hoang <bawsing@gmail.com>
2018-02-28 17:29:24 -0800

    Rename City to CityComponent to prevent conflict with the type

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Cities from './components/Cities';
-import City from './components/City';
+import CityComponent from './components/City';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Switch>
-          <Route exact={true} path="/" component={Cities}/>
-          <Route path="/cities/:id" component={City}/>
+          <Route exact={true} path="/" component={Cities} />
+          <Route path="/cities/:id" component={CityComponent}/>
         </Switch>
       </div>
     );

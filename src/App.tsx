1843a1584547c84c5bb5637d45f8150a36c9d567
Louis Hoang <bawsing@gmail.com>
2018-02-28 17:26:00 -0800

    Minor

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
 import * as React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Cities from './components/Cities';
-import { City } from './types';
-
-const cities: City[] = [
-  { id: 1, 
-    name: 'San Francisco', 
-    lat: 37.562992, 
-    long: -122.325525, 
-    description: 'moderate rain',
-    icon: 'https://cdn.glitch.com/6e8889e5-7a72-48f0-a061-863548450de5%2F10n.png?1499366021399',
-    temp: 14.96
-  },
-  {
-    id: 2,
-    name: 'Hayward',
-    lat: 37.668821,
-    long: -122.080796,
-    icon: 'https://cdn.glitch.com/6e8889e5-7a72-48f0-a061-863548450de5%2F04d.png?1499366020964',
-    temp: 13.09,
-    description: 'overcast clouds'
-  }
-]
+import City from './components/City';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Cities/>
+        <Switch>
+          <Route exact={true} path="/" component={Cities}/>
+          <Route path="/cities/:id" component={City}/>
+        </Switch>
       </div>
     );
   }

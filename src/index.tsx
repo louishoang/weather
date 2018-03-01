f0af12d74d30d0a7a72fc45ca766fc070b4519e6
Louis Hoang <bawsing@gmail.com>
2018-02-28 17:25:41 -0800

    Use Router, Switch to go from cities page to city show page

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,15 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { Router } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+const history = createHistory();
 
 ReactDOM.render(
-  <App />,
+  <Router history={history}>
+    <App />
+  </Router>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
